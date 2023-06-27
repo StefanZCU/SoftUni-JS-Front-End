0fe57ebfeb13148f66d4616c8433ffef4705dbed
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-06-27 12:14:15 +0300

    Added 15. Censored Words

---
+++ JS-Front-End/1.1 JS Syntax Fundamentals - Lab/15. Censored Words/Program.js
@@ -0,0 +1,15 @@
+function solve (text, word){
+    let censored = text.replace(word, repeat(word));
+    while(censored.includes(word)){
+        censored = censored.replace(word, repeat(word));
+    }
+
+    console.log(censored);
+
+    function repeat(word){
+        let star = '*';
+        return star.repeat(word.length);
+    }
+}
+
+solve('A small sentence with some words', 'small')
