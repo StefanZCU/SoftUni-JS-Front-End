9e96a99e3e0fb67991af752c011c6c139f09fe1b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-06-28 11:37:03 +0300

    Update Program.js

--- JS-Front-End/1.2 JS Syntax Fundamentals - Exercises/14. Print every N-th Element from an Array/Program.js
+++ JS-Front-End/1.2 JS Syntax Fundamentals - Exercises/14. Print every N-th Element from an Array/Program.js
@@ -1,6 +1,6 @@
 function solve(stringArr, nthWord){
     let newArray = [];
-    for(i = 0; i < stringArr.length; i += nthWord){
+    for(let i = 0; i < stringArr.length; i += nthWord){
         newArray.push(stringArr[i]);
     }
 
