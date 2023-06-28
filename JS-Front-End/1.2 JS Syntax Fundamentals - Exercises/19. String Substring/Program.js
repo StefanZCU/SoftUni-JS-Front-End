e06fbf275cad4fd716fd1449a7cf59698a9bcb2b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-06-28 15:18:19 +0300

    Added 19. String Substring

--- JS-Front-End/1.2 JS Syntax Fundamentals - Exercises/19. String Substring/Program.js
+++ JS-Front-End/1.2 JS Syntax Fundamentals - Exercises/19. String Substring/Program.js
@@ -1,8 +1,15 @@
-function solve(wordToFind, text){
-    if(text.toLowerCase().includes(wordToFind.toLowerCase())){
-        console.log(wordToFind);
+function solve(wordToFind, text) {
+    let array = text.split(" ");
+  
+    array = array.filter(function (word) {
+      if (word.toLowerCase() === wordToFind.toLowerCase()) {
+        return word;
+      }
+    });
+  
+    if (array.length > 0) {
+      console.log(wordToFind);
+    } else {
+      console.log(`${wordToFind} not found!`);
     }
-    else{
-        console.log(`${wordToFind} not found!`)
-    }
-}
+  }
