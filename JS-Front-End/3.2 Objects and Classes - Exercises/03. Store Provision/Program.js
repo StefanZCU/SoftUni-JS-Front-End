2bbef2afe6899f475f165acb3fc2ca4df7670d75
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-07-20 12:50:15 +0300

    Added 03. Store Provision

---
+++ JS-Front-End/3.2 Objects and Classes - Exercises/03. Store Provision/Program.js
@@ -0,0 +1,21 @@
+function updateStockAndPrint(storeStock, deliveryProducts) {
+    const stock = {};
+
+    for (let i = 0; i < storeStock.length; i += 2) {
+        const product = storeStock[i];
+        const quantity = parseInt(storeStock[i + 1]);
+
+        stock[product] = (stock[product] || 0) + quantity;
+    }
+
+    for (let i = 0; i < deliveryProducts.length; i += 2) {
+        const product = deliveryProducts[i];
+        const quantity = parseInt(deliveryProducts[i + 1]);
+
+        stock[product] = (stock[product] || 0) + quantity;
+    }
+
+    for (const product in stock) {
+        console.log(`${product} -> ${stock[product]}`);
+    }
+}
