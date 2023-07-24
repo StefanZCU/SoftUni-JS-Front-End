4990cb52d306970954438105c88e23844875aec6
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-07-24 12:33:21 +0300

    Added 07. Colorize Table

---
+++ JS-Front-End/4.1 DOM and Events - Lab/07. Colorized-Table/ColorizeTable.js
@@ -0,0 +1,7 @@
+function colorize() {
+    const tableRows = document.querySelectorAll('table tr');
+
+    for (let i = 1; i < tableRows.length; i += 2) {
+        tableRows[i].style.backgroundColor = 'Teal';
+    }
+}
