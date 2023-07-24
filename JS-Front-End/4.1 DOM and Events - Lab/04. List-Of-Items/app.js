963ae1af74f7b7689bfcda2075bbfe62ff8aa09b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-07-24 12:08:37 +0300

    Added 04. List of Items

---
+++ JS-Front-End/4.1 DOM and Events - Lab/04. List-Of-Items/app.js
@@ -0,0 +1,7 @@
+function addItem() {
+    const textToAdd = document.getElementById('newItemText').value;
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(textToAdd));
+    document.getElementById('items').appendChild(li);
+    document.getElementById('newItemText').value = '';
+}
