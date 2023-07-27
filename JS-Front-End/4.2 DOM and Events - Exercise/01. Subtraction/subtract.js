f78fce5354ccdd93bec772d168acb8ab565590d6
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2023-07-27 14:04:09 +0300

    Added 01. Subtraction

--- JS-Front-End/4.2 DOM and Events - Exercise/01. Subtraction/subtract.js
+++ JS-Front-End/4.2 DOM and Events - Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstNum = Number(document.getElementById("firstNumber").getAttribute('value'));
+    const secondNum = Number(document.getElementById("secondNumber").getAttribute('value'));
+
+    document.getElementById("result").textContent += Number(firstNum - secondNum);
+
+
 }
