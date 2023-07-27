3c149aecbe389d0fb339e72239834e2657aa126d
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2023-07-27 15:23:54 +0300

    Added 03. Accordion

--- JS-Front-End/4.2 DOM and Events - Exercise/03. Accordion/accordion.js
+++ JS-Front-End/4.2 DOM and Events - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    const buttontext = document.getElementsByClassName("button")[0];
+    const extraInfo = document.getElementById("extra");
+
+        if (buttontext.textContent === "More"){
+            buttontext.textContent = "Less";
+            extraInfo.style.display = "block";
+        } else if (buttontext.textContent === "Less") {
+            buttontext.textContent = "More";
+            extraInfo.style.display = "none";
+        }
+
 }
