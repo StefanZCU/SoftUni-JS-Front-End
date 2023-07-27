683a628a501a7764233bea15531c99910ae78d44
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2023-07-27 13:43:14 +0300

    Added 09. Highlight Active

---
+++ JS-Front-End/4.1 DOM and Events - Lab/09. Highlight-Active/app.js
@@ -0,0 +1,14 @@
+function focused() {
+    const inputs = Array.from(document.querySelectorAll('input'));
+
+    for (const input of inputs) {
+
+        input.addEventListener('focus', (e) =>{
+            e.target.parentElement.className = 'focused';
+        });
+
+        input.addEventListener('blur', (e) =>{
+            e.target.parentElement.className = '';
+        });
+    }
+}
